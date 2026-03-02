ea3b1addb9a38892546567f6f8e5253a6ed74080
cgamcs <cesar.garciamcs@proton.me>
2026-03-02 14:46:30 -0600

    actualizando referencia de tareas en el proyecto

--- backend/src/controllers/TaskController.ts
+++ backend/src/controllers/TaskController.ts
@@ -63,10 +63,8 @@ export class TaskController {
       if(!task) {
         return res.status(404).json({ error: 'Tarea no encontrada' })
       }
-      if(task.project.toString() !== req.project._id.toString()) {
-        return res.status(400).json({ error: 'Acción no valida' })
-      }
-      await task.deleteOne()
+      req.project.tasks = req.project.tasks.filter(task => task.toString() !== taskId)
+      await Promise.allSettled([ task.deleteOne(), req.project.save() ])
       res.send("Tarea eliminada!")
     } catch (error) {
       console.log(error)
