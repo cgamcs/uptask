a501cb541bd548a98c7fbe87dd9fe50e0257a199
cgamcs <cesar.garciamcs@proton.me>
2026-03-05 09:29:51 -0600

    agregando menu de hamburguesa

--- frontend/src/layouts/AppLayout.tsx
+++ frontend/src/layouts/AppLayout.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router-dom"
 import Logo from "@/components/Logo"
+import NavMenu from "@/components/NavMenu"
 
 function AppLayout() {
   return (
     <>
       <header className="bg-gray-800">
-        <div className="max-w-screen-2xl mx-auto flex flex-col lg:flex-row justify-between items-center">
+        <div className="max-w-screen-2xl mx-auto flex px-5 lg:px-0 flex-row justify-between items-center">
           <div className="w-64">
             <Logo />
           </div>
+
+          <NavMenu />
         </div>
       </header>
 
