1d82f3632499142d07778cca20fced0bbf5f428a
cgamcs <cesar.garciamcs@proton.me>
2026-03-06 11:54:12 -0600

    trayendo los proyectos y mostrandolos en la pagina

--- frontend/src/api/ProjectAPI.ts
+++ frontend/src/api/ProjectAPI.ts
@@ -1,12 +1,25 @@
 import api from "@/lib/axios"
-import type { ProjectFormData } from "@/types"
+import { dashboardProjectSchema, type ProjectFormData } from "@/types"
 import { isAxiosError } from "axios"
 
 export async function createProject(formData : ProjectFormData) {
-  console.log(formData)
   try {
     const { data } = await api.post('/projects', formData)
     return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function getProjects() {
+  try {
+    const { data } = await api('/projects')
+    const response = dashboardProjectSchema.safeParse(data)
+    if(response.success) {
+      return response.data
+    }
   } catch (error) {
     if(isAxiosError(error) && error.response) {
       throw new Error(error.response.data.error)
