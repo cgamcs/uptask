98813d980f65b728984a85210fe413cc5edf036a
cgamcs <cesar.garciamcs@proton.me>
2026-03-09 18:42:20 -0600

    ruta para crear usuario con comprobacion de contrasena y hasheo

--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -1,9 +1,22 @@
 import { Router } from "express"
+import { AuthController } from "../controllers/AuthController"
+import { body } from "express-validator"
+import { handleInputErrors } from "../middleware/validation"
 
 const router = Router()
 
-router.get('/', (req, res) => {
-  res.send('desde "/api/auth"')
-})
+router.post('/create-account',
+  body('name').notEmpty().withMessage('El nombre no puede ir vacio'),
+  body('password').isLength({min: 16}).withMessage('La contraseña es muy corta, minimo 16 caracteres'),
+  body('password_confirmation').custom((value, {req}) => {
+    if (value !== req.body.password) {
+      throw new Error('Las contraseñas son diferentes')
+    }
+    return true
+  }),
+  body('email').isEmail().withMessage('Email no valido'),
+  handleInputErrors,
+  AuthController.createAccount
+)
 
 export default router
