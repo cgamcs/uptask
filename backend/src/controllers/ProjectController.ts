a56bae98ee057c032655ce7d932bc243d0546a87
cgamcs <cesar.garciamcs@proton.me>
2026-03-12 08:25:02 -0600

    dando permisos a los miembros del equipo para ver los proyectos a de los cuales son miembros

--- backend/src/controllers/ProjectController.ts
+++ backend/src/controllers/ProjectController.ts
@@ -20,7 +20,8 @@ export class ProjectController {
     try {
       const projects = await Project.find({
         $or: [
-          {manager: {$in: req.user._id}} // Solo obtine los porjectos que le pertenecen al usuario autenticado
+          {manager: {$in: req.user._id}}, // Solo obtine los porjectos que le pertenecen al usuario autenticado
+          {team: {$in: [req.user._id]}}
         ]
       })
       res.json(projects)
@@ -37,7 +38,7 @@ export class ProjectController {
         return res.status(404).json({ error: 'Proyecto no encontrado' })
       }
 
-      if(project.manager.toString() !== req.user._id.toString()) {
+      if(project.manager.toString() !== req.user._id.toString() && !project.team.includes(req.user._id)) {
         return res.status(404).json({ error: 'Accion no valida' })
       }
 
