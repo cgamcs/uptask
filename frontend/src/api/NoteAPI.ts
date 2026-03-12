782a02fb093bc6a4b1d58b552605106950325e22
cgamcs <cesar.garciamcs@proton.me>
2026-03-12 14:57:44 -0600

    eliminando notas si eres el dueno y refrescando con queryClient

--- frontend/src/api/NoteAPI.ts
+++ frontend/src/api/NoteAPI.ts
@@ -1,4 +1,4 @@
-import { type NoteFormData, type Project, type Task } from "@/types"
+import { type Note, type NoteFormData, type Project, type Task } from "@/types"
 import { isAxiosError } from "axios"
 import api from "@/lib/axios"
 
@@ -6,12 +6,25 @@ type NoteAPIType = {
   formData: NoteFormData
   projectId: Project['_id']
   taskId: Task['_id']
+  noteId: Note['_id']
 }
 
 export async function createNote({projectId, taskId, formData} : Pick<NoteAPIType, 'projectId' | 'taskId' | 'formData'>) {
   try {
     const url = `/projects/${projectId}/tasks/${taskId}/notes`
-    const { data } = await api.post(url, formData)
+    const { data } = await api.post<string>(url, formData)
+    return data
+  } catch (error) {
+    if(isAxiosError(error) && error.response) {
+      throw new Error(error.response.data.error)
+    }
+  }
+}
+
+export async function removeNote({projectId, taskId, noteId} : Pick<NoteAPIType, 'projectId' | 'taskId' | 'noteId'>) {
+  try {
+    const url = `/projects/${projectId}/tasks/${taskId}/notes/${noteId}`
+    const { data } = await api.delete<string>(url)
     return data
   } catch (error) {
     if(isAxiosError(error) && error.response) {
--- frontend/src/components/notes/NoteDetail.tsx
+++ frontend/src/components/notes/NoteDetail.tsx
@@ -1,17 +1,58 @@
-import type { Note } from "@/types";
-import { formatDate } from "@/utils/utils";
+import { removeNote } from "@/api/NoteAPI"
+import { useAuth } from "@/hooks/useAuth"
+import type { Note } from "@/types"
+import { formatDate } from "@/utils/utils"
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { useMemo } from "react"
+import { useLocation, useParams } from "react-router-dom"
+import { toast } from "sonner"
 
 type NoteDetailProps = {
   note: Note
 }
 
 function NoteDetail({note}: NoteDetailProps) {
+  const params = useParams()
+  const location = useLocation()
+
+  const projectId = params.projectId!
+  const queryParams = new URLSearchParams(location.search)
+  const taskId = queryParams.get('viewTask')!
+
+  const { data, isLoading } = useAuth()
+  const canDelete = useMemo(() => data?._id === note.createdBy._id, [data])
+
+  const queryClient = useQueryClient()
+
+  const { mutate } = useMutation({
+    mutationFn: removeNote,
+    onError: (error) => {
+      toast.error(error.message)
+    },
+    onSuccess: (data) => {
+      toast.success(data)
+      queryClient.invalidateQueries({queryKey: ['task', taskId]})
+    }
+  })
+
+  if (isLoading) return <span className="w-full text-center h-fit">Cargando...</span>
+
   return (
-   <div className="p-3 flex justify-between items-center">
+   <div className="p-3 flex gap-5 justify-between items-center">
     <div>
       <p><span className="font-bold">{note.createdBy.name}:</span> {note.content}</p>
-      <p className="text-gray-500">{formatDate(note.createdAt)}</p>
+      <p className="text-gray-500 mt-2">{formatDate(note.createdAt)}</p>
     </div>
+
+    {canDelete && (
+      <button
+        type="button"
+        className="text-red-500 hover:text-red-600 cursor-pointer active:scale-95 transition-all ease-linear"
+        onClick={() => mutate({projectId, taskId, noteId: note._id})}
+      >
+        Eliminar
+      </button>
+    )}
    </div>
   )
 }
