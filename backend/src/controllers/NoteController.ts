df7655bcc1c9425fa49db0d669b317d458282701
cgamcs <cesar.garciamcs@proton.me>
2026-03-12 11:34:58 -0600

    creando controlador para obtener las notas de una tarea y haciendole populate desde taskController, ademas de un controlador para eliminar las notas solo si eres el que creo la nota

--- backend/src/controllers/NoteController.ts
+++ backend/src/controllers/NoteController.ts
@@ -1,5 +1,10 @@
+import { Types } from "mongoose"
 import { Request, Response } from "express"
 import Note, { INote } from "../models/Notes"
+
+type NoteParamas = {
+  noteId: Types.ObjectId
+}
 
 export class NoteController {
   static createNote = async (req: Request<{}, {}, INote>, res: Response) => {
@@ -15,6 +20,39 @@ export class NoteController {
     try {
       await Promise.allSettled([ req.task.save(), note.save() ])
       res.send('Nota creada!')
+    } catch (error) {
+      res.status(500).json({ error: 'Hubo un error' })
+    }
+  }
+
+  static getTaskNotes = async (req: Request, res: Response) => {
+    try {
+      const notes = await Note.find({ task: req.task._id })
+      res.json(notes)
+    } catch (error) {
+      res.status(500).json({ error: 'Hubo un error' })
+    }
+  }
+
+  static removeNote = async (req: Request<NoteParamas>, res: Response) => {
+    const { noteId } = req.params
+
+    const note = await Note.findById(noteId)
+    if(!note) {
+      const error = new Error('Nota no encontrada')
+      return res.status(404).json({ error: error.message })
+    }
+
+    if(note.createdBy.toString() !== req.user._id.toString()) {
+      const error = new Error('Acción no valida')
+      return res.status(401).json({ error: error.message })
+    }
+
+    req.task.notes = req.task.notes.filter(note => note.toString() !== noteId.toString())
+
+    try {
+      await Promise.allSettled([ req.task.save(), note.deleteOne() ])
+      res.send('Nota eliminada!')
     } catch (error) {
       res.status(500).json({ error: 'Hubo un error' })
     }
--- backend/src/routes/projectRouter.ts
+++ backend/src/routes/projectRouter.ts
@@ -119,4 +119,14 @@ router.post('/:projectId/tasks/:taskId/notes',
   NoteController.createNote
 )
 
+router.get('/:projectId/tasks/:taskId/notes',
+  NoteController.getTaskNotes
+)
+
+router.delete('/:projectId/tasks/:taskId/notes/:noteId',
+  param('noteId').isMongoId().withMessage('ID no valido'),
+  handleInputErrors,
+  NoteController.removeNote
+)
+
 export default router
