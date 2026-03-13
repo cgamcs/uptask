0c50fe1e6e9eedfc87597a32c54eaa379e02974a
cgamcs <cesar.garciamcs@proton.me>
2026-03-13 09:52:03 -0600

    cambiando estilo al modal para agregar una tarea

--- frontend/src/components/tasks/AddTaskModal.tsx
+++ frontend/src/components/tasks/AddTaskModal.tsx
@@ -99,7 +99,7 @@ export default function AddTaskModal() {
                     <input 
                       type="submit"
                       value="Crear Tarea"
-                      className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 mt-5 text-white uppercase font-bold cursor-pointer active:scale-95 transition-all ease-linear"
+                      className="bg-fuchsia-600 hover:bg-fuchsia-700 w-full p-3 mt-5 text-white uppercase font-bold cursor-pointer active:scale-[0.97] transition-all ease-linear rounded-xl"
                     />
                   </form>
                 </Dialog.Panel>
--- frontend/src/components/tasks/TaskForm.tsx
+++ frontend/src/components/tasks/TaskForm.tsx
@@ -11,36 +11,40 @@ export default function TaskForm({ errors, register }: TaskFormProps) {
   return (
     <>
       <div className="flex flex-col gap-5">
-        <label className="font-normal text-2xl" htmlFor="name">
-          Nombre de la tarea
-        </label>
+        <div className="flex justify-between items-center">
+          <label className="font-normal text-2xl" htmlFor="name">
+            Nombre de la tarea
+          </label>
+          {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
+        </div>
         <input
           id="name"
           type="text"
           placeholder="Nombre de la tarea"
-          className="w-full p-3  border-gray-300 border"
+          className="w-full p-3  border-gray-300 border rounded-xl"
           {...register("name", {
             required: "El nombre de la tarea es obligatorio",
           })}
         />
-        {errors.name && <ErrorMessage>{errors.name.message}</ErrorMessage>}
       </div>
 
       <div className="flex flex-col gap-5">
-        <label className="font-normal text-2xl" htmlFor="description">
-          Descripción de la tarea
-        </label>
+        <div className="flex justify-between items-center">
+          <label className="font-normal text-2xl" htmlFor="description">
+            Descripción de la tarea
+          </label>
+          {errors.description && (
+            <ErrorMessage>{errors.description.message}</ErrorMessage>
+          )}
+        </div>
         <textarea
           id="description"
           placeholder="Descripción de la tarea"
-          className="w-full p-3  border-gray-300 border"
+          className="w-full p-3  border-gray-300 border rounded-xl"
           {...register("description", {
             required: "La descripción de la tarea es obligatoria",
           })}
         />
-        {errors.description && (
-          <ErrorMessage>{errors.description.message}</ErrorMessage>
-        )}
       </div>
     </>
   )
